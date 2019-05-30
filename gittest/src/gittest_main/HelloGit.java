c4eb08461c4a8bc3026ae2803aaf7d24b6e8a6b1
test <wowywnx@163.com>
2019-05-30 09:45:47 +0800

    committing now...2019/5/30 09:45:00

---
+++ gittest/src/gittest_main/HelloGit.java
@@ -0,0 +1,13 @@
+package gittest_main;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello everyone,this is my first git project...");
+		
+		System.out.println("I am GoonerForever...");
+
+	}
+
+}
